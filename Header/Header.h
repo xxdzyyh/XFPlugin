56d3761328dcd72e7d980d30f8abf1429f64ba1c
xxdzyyh <591392966@qq.com>
2016-10-21 19:13:18 +0800

    从XFFoundation中独立出来

---
+++ Header/Header.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  XFPlugin
+//
+//  Created by wangxuefeng on 16/10/20.
+//  Copyright © 2016年 wangxuefeng. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "XFPlugin.h"
+#import "XFAppDelegate"
+#import "XFPluginManager.h"
+
+#endif /* Header_h */
---
+++ XFPlugin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XFPlugin
+//
+//  Created by wangxuefeng on 16/10/20.
+//  Copyright © 2016年 wangxuefeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
